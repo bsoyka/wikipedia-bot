551590d83a204686da8e85d908626beea173f65c
Ben Soyka <hello@bsoyka.me>
2023-12-28 18:02:30 -0700

    Create fix_proxy_urls.py

---
+++ fix_proxy_urls.py
@@ -0,0 +1,30 @@
+import pywikibot
+from pywikibot import pagegenerators
+
+REPLACEMENTS = {
+    "www-newspapers-com.wikipedialibrary.idm.oclc.org": "www.newspapers.com"
+}
+
+site = pywikibot.Site()
+
+pages_to_edit: set[pywikibot.Page] = set()
+
+for proxy_string in REPLACEMENTS:
+    new_pages = set(
+        pagegenerators.SearchPageGenerator(
+            f'insource:"{proxy_string}"',
+            namespaces={
+                0,
+            },
+        )
+    )
+    pages_to_edit.update(new_pages)
+
+for page in pages_to_edit:
+    for find, replace in REPLACEMENTS.items():
+        page.text = page.text.replace(find, replace)
+
+    page.save(
+        "Replacing [[WP:TWL|TWL]] proxy links ([[User:BsoykaBot/Task 2|Task 2]])",
+        minor=True,
+    )
