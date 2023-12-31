726759f914cb836c59f66532c5f66cd8fbd993af
Ben Soyka <hello@bsoyka.me>
2023-12-31 14:18:38 -0700

    Move script to function

--- tasks/fix_proxy_urls.py
+++ tasks/fix_proxy_urls.py
@@ -11,26 +11,32 @@ REPLACEMENTS = {
     "www.newspapers.com.wikipedialibrary.idm.oclc.org": "www.newspapers.com",
 }
 
-site = pywikibot.Site()
 
-pages_to_edit: set[pywikibot.Page] = set()
+def main():
+    site = pywikibot.Site()
 
-for proxy_string in REPLACEMENTS:
-    new_pages = set(
-        pagegenerators.SearchPageGenerator(
-            f'insource:"{proxy_string}"',
-            namespaces={
-                0,
-            },
+    pages_to_edit: set[pywikibot.Page] = set()
+
+    for proxy_string in REPLACEMENTS:
+        new_pages = set(
+            pagegenerators.SearchPageGenerator(
+                f'insource:"{proxy_string}"',
+                namespaces={
+                    0,
+                },
+            )
         )
-    )
-    pages_to_edit.update(new_pages)
+        pages_to_edit.update(new_pages)
 
-for page in pages_to_edit:
-    for find, replace in REPLACEMENTS.items():
-        page.text = page.text.replace(find, replace)
+    for page in pages_to_edit:
+        for find, replace in REPLACEMENTS.items():
+            page.text = page.text.replace(find, replace)
 
-    page.save(
-        "Replacing [[WP:TWL|TWL]] proxy links ([[User:BsoykaBot/Task 2|Task 2]])",
-        minor=True,
-    )
+        page.save(
+            "Replacing [[WP:TWL|TWL]] proxy links ([[User:BsoykaBot/Task 2|Task 2]])",
+            minor=True,
+        )
+
+
+if __name__ == "__main__":
+    main()
