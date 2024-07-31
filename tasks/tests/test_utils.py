4d79c31f4f8c63a6de49d117748b722120a558f6
Ben Soyka <hello@bsoyka.me>
2024-07-31 09:42:55 -0600

    Fix edit summary util test

--- tasks/tests/test_utils.py
+++ tasks/tests/test_utils.py
@@ -1,8 +1,9 @@
+from tasks import __version__
 from tasks._utils import create_edit_summary
 
 
 def test_create_edit_summary():
     assert (
-        create_edit_summary("Fixing links", 1, "0.3.0")
-        == "Fixing links ([[User:BsoykaBot/Task 1|Task 1]] v0.3.0, [[User talk:BsoykaBot|report errors]])"
+        create_edit_summary("Fixing links", task=1)
+        == f"Fixing links ([[User:BsoykaBot/Task 1|Task 1]], v{__version__}, [[User talk:BsoykaBot|report errors]])"
     )
