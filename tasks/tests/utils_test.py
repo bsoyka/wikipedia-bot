476b6546f6690d6377bc1a0995a25a551b8adc41
Ben Soyka <hello@bsoyka.me>
2025-07-07 17:26:19 -0600

    Enable/pass ruff E rules

--- tasks/tests/utils_test.py
+++ tasks/tests/utils_test.py
@@ -6,7 +6,7 @@ from tasks._utils import create_edit_summary
 
 def test_create_edit_summary() -> None:
     """Test that create_edit_summary generates the correct summary."""
-    assert (
-        create_edit_summary("Fixing links", task=1)
-        == f"Fixing links ([[User:BsoykaBot/Task 1|Task 1]], v{__version__}, [[User talk:BsoykaBot|report errors]])"
+    assert create_edit_summary("Fixing links", task=1) == (
+        f"Fixing links ([[User:BsoykaBot/Task 1|Task 1]], v{__version__},"
+        "[[User talk:BsoykaBot|report errors]])"
     )
